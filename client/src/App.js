8937d62307f71a5da580a304a19768a85c083fc2
facundo-santacruz <facugs2090@gmail.com>
2020-11-04 16:42:13 -0300

    Agregando Containers: Nav - Catalog e instale react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import SimpleContainer from './Containers/Catalog';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <SimpleContainer></SimpleContainer>
     </div>
   );
 }
