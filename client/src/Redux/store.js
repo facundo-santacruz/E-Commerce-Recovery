8b47f43f17414f998b67f28a107265e81f96bb67
facundo-santacruz <facugs2090@gmail.com>
2020-11-05 02:47:16 -0300

    Agregando librerias redux redux-devtools-extension-reux-thunk y congigurando el store

---
+++ client/src/Redux/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from "./reducer/index.js";
+import thunk from 'redux-thunk';
+import { saveState } from './reducer/localStorage';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+
+export default store;
