d82f997cbff72d294bc98c3f3ff2dc9ab081237e
facundo-santacruz <facugs2090@gmail.com>
2020-11-20 12:43:33 -0300

    primer filtrado funcionando API2

--- api/index.js
+++ api/index.js
@@ -335,7 +335,43 @@ app.get('/api/conditioncat', function(req, res) {
 });
 
 
-https://api.mercadolibre.com/sites/MLA/search?category=MLA1055
+
+app.get('/api/filter', function(req, res) {    
+  console.log(req.query);
+  try {
+    const { search, value, number, categoria } = req.query;
+    // Check the redis store for the data first
+    client.get(`${search} ${categoria}=${value}${number}`, async (err, recipe) => {
+      if (recipe) {
+        console.log("si existe en la cache");
+        return res.status(200).send({
+          error: false,
+          message: `Recipe for query:${search} ${categoria}=${value} offset:${number} from the cache`,
+          data: JSON.parse(recipe)
+        })
+      } else { // When the data is not found in the cache then we can make request to the server
+        console.log("no existe en la cache");
+        const recipe = await axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${search}&${categoria}=${value}&offset=${number}&limit=${30}`);
+
+        // save the record in the cache for subsequent request
+        client.setex(`${search} ${categoria}=${value}${number}`, 20000, JSON.stringify(recipe.data));
+
+        // return the result to the client
+        return res.status(200).send({
+          error: false,
+          message: `Recipe for query:${search} ${categoria}=${value} offset:${number} from the server`,
+          data: recipe.data
+        });
+    }
+  }) 
+} catch (error) {
+    console.log(error)
+}
+});
+
+
+
+// https://api.mercadolibre.com/sites/MLA/search?category=MLA1055
 
 app.get('/', (req, res) => {
   res.status(200).send("Welcome to MERCADO LIBRE'S API REST")
