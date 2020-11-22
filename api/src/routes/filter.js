6e979140e8112a86fa572e46580438ed42ff59cd
facundo-santacruz <facugs2090@gmail.com>
2020-11-22 15:23:25 -0300

    creada la ruta filter

---
+++ api/src/routes/filter.js
@@ -0,0 +1,46 @@
+const app = require('express').Router();
+const axios = require('axios')
+const cat = require('../models/categories')
+
+const redis = require('redis');
+// make a connection to the local instance of redis
+const client = redis.createClient(6379);
+
+
+client.on("error", (error) => {
+  console.error(error);
+})
+
+
+app.get('/api/filter', function(req, res) {    
+    console.log(req.query);
+        try {
+        const { search, value, number, categoria } = req.query;
+        // Check the redis store for the data first
+        client.get(`${search} ${categoria}=${value}${number}`, async (err, recipe) => {
+            if (recipe) {
+            console.log("si existe en la cache");
+            return res.status(200).send({
+                error: false,
+                message: `Recipe for query:${search} ${categoria}=${value} offset:${number} from the cache`,
+                data: JSON.parse(recipe)
+            })
+            } else { // When the data is not found in the cache then we can make request to the server
+            console.log("no existe en la cache");
+            const recipe = await axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${search}&${categoria}=${value}&offset=${number}&limit=${30}`);
+
+            // save the record in the cache for subsequent request
+            client.setex(`${search} ${categoria}=${value}${number}`, 20000, JSON.stringify(recipe.data));
+
+            // return the result to the client
+            return res.status(200).send({
+                error: false,
+                message: `Recipe for query:${search} ${categoria}=${value} offset:${number} from the server`,
+                data: recipe.data
+            });
+        }
+        }) 
+    } catch (error) {
+        console.log(error)
+    }
+});
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -29,14 +29,16 @@
 // }
 
 const { Router } = require('express');
-const productRouter = require('./product.js')
-const categoryRouter = require('./category.js')
+const productRouter = require('./product.js');
+const categoryRouter = require('./category.js');
+const filterRouter = require('./filter.js');
 const router = Router();
 
 
 
 router.use('/products', productRouter);
 router.use('/category', categoryRouter);
+router.use('/filter', filterRouter)
 // router.get('/', (req, res) => {
 //   res.send("Hi World")
 // })
