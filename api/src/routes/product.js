570f9baaff6104fc3a57de5ecc2d6a7d09b95656
facundo-santacruz <facugs2090@gmail.com>
2021-03-30 05:15:20 -0300

    (api/products) cleaning code

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,21 +1,7 @@
 const app = require('express').Router();
 const axios = require('axios')
 
-const { response } = require('express')
 const  {getProductsRedis } = require('../redis')
-// const redis = require('ioredis');
-// const client = new redis(process.env.STACKHERO_REDIS_URL_TLS);
-// client.on("error", (error) => {
-//   console.error(error);
-// })
-
-// client.get(`ram1`, function (err, result) {
-//   if (err) {
-//     console.error(err);
-//   } else {
-//     console.log(result); // Promise resolves to "bar"
-//   }
-// });
 // //----------------------BUSCAR UN PRODUCTO------------------------------------------------------
 
 app.get('/product', function(req, res) {    
@@ -29,8 +15,7 @@ app.get('/product', function(req, res) {
 
 
 // ---------------REALIZA LA PRIMERA BUSQUEDA DE PRODUCTO----------
-app.get('/search', function(req, res) {    
-  console.log(req.query);
+app.get('/search', function(req, res) { 
   const { search, number } = req.query;
   const route = `https://api.mercadolibre.com/sites/MLA/search?q=${search}&offset=${(number-1)*30}&limit=${30}`;
   return getProductsRedis(`${search}${number}`, res, route, "query")
@@ -41,32 +26,15 @@ const objeto = (myObj) => {
   var newObj = ""
   for (var key in myObj) {
     if (key !== "search" && key !== "number"){
-      console.log(myObj[key][1]);
       typeof(myObj[key]) === 'string'  ? newObj+=`&${key}=${myObj[key].replace(",","")}` : newObj+=`&${key}=${myObj[key][1].replace(","," ")}`
     }
   }
-  console.log(newObj)
   return newObj;
 }
-
-const cant = (array) => {
-  const newArray = array.map((elem, i) => {
-    if (elem[1].length===1){
-      return `&${elem[0]}=${elem[1]}`
-    }else{
-      return elem[1].map(sub => `&${elem[0]}=${sub}`)
-    }
-
-  })
-  return newArray
-  }
 //----------------------BUSCAR POR CONDICION (usado o no)------------------------------------------------------
 
 app.get('/condition', function(req, res) {
-  console.log(req.query)    
   var condition = objeto(req.query)
-  // const condition = cant(filtros)
-  console.log(condition);
   const { search, number} = req.query
   const route = `https://api.mercadolibre.com/sites/MLA/search?q=${search}&offset=${(number-1)*30}&limit=${30}${condition}`
   return getProductsRedis(`${search}${number}${condition}`, res, route, "query")
@@ -76,7 +44,6 @@ app.get('/condition', function(req, res) {
 //-------------------BUSCAAR REVIEW  -----------------------
 app.get('/reviews', function(req, res) {    
   const { id } = req.query
-  console.log(req.id)
   const route = `https://api.mercadolibre.com/reviews/item/${id}`
   return getProductsRedis(`review${id}`, res, route, "query")
 });
